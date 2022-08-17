dafe7d7cb5e0e531f25a708c65a1d17b665ebbd5
Isaiah <ijandicos90@gmail.com>
2022-08-17 00:00:06 -0400

    Completed lab

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,6 +3,18 @@ class StudentsController < ApplicationController
   def index
     students = Student.all
     render json: students
+
+#     students = if params[:name]
+#       Student.by_name(params[:name])
+#     else
+#       Student.all
+#     end    
+# render json: students
+  end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
